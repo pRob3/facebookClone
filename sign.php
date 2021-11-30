061675b5f7e68f8fd55cc2914a26f0f06cf910b0
pRob3 <bubblan@slaf.se>
2021-11-30 08:08:08 +0100

    Designed log in form

--- sign.php
+++ sign.php
@@ -1,10 +1,10 @@
 <?php
 
-require'connect/DB.php';
-require'core/load.php';
+require 'connect/DB.php';
+require 'core/load.php';
 
 
-if( isset($_POST['first-name']) && !empty($_POST['first-name'])){
+if (isset($_POST['first-name']) && !empty($_POST['first-name'])) {
 
     $upFirst = $_POST['first-name'];
 
@@ -20,83 +20,81 @@ if( isset($_POST['first-name']) && !empty($_POST['first-name'])){
 
     $birthYear = $_POST['birth-year'];
 
-    if(!empty($_POST['gen'])){
+    if (!empty($_POST['gen'])) {
         $upgen = $_POST['gen'];
     }
 
-    $birth = ''.$birthYear.'-'.$birthMonth.'-'.$birthDay.'';
+    $birth = '' . $birthYear . '-' . $birthMonth . '-' . $birthDay . '';
 
-    if(empty($upFirst) or empty($upLast) or empty($upEmailMobile) or empty($upgen)){
+    if (empty($upFirst) or empty($upLast) or empty($upEmailMobile) or empty($upgen)) {
 
-            $error = 'All feilds are required';
-    }else{
+        $error = 'All feilds are required';
+    } else {
         $first_name = $loadFromUser->checkInput($upFirst);
         $last_name = $loadFromUser->checkInput($upLast);
         $email_mobile = $loadFromUser->checkInput($upEmailMobile);
         $password = $loadFromUser->checkInput($upPassword);
-        $screenName = ''.$first_name.'_'.$last_name.'';
-        
-        if(DB::query('SELECT screenName FROM users WHERE screenName = :screenName', array(':screenName' => $screenName))){
+        $screenName = '' . $first_name . '_' . $last_name . '';
+
+        if (DB::query('SELECT screenName FROM users WHERE screenName = :screenName', array(':screenName' => $screenName))) {
             $screenRand = rand();
-            $userLink = ''. $screenName.''. $screenRand.'';
-        }else{
+            $userLink = '' . $screenName . '' . $screenRand . '';
+        } else {
             $userLink = $screenName;
         }
-        
-        if(!preg_match("^[_a-z0-9-]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^",$email_mobile)){
-            if(!preg_match("^[0-9]{11}^", $email_mobile)){
+
+        if (!preg_match("^[_a-z0-9-]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^", $email_mobile)) {
+            if (!preg_match("^[0-9]{11}^", $email_mobile)) {
                 $error = 'Email or mobile number is not correct. Please try again.';
-            }else{
+            } else {
                 // Mobile number
                 $mob = strlen((string)$email_mobile);
-                
-                if($mob > 11 || $mob < 11){
+
+                if ($mob > 11 || $mob < 11) {
                     $error = 'Mobile number is not valid';
-                }else if(strlen($password) <5 || strlen($password) >= 60){
+                } else if (strlen($password) < 5 || strlen($password) >= 60) {
                     $error = 'The password is either too short ot too long';
-                }else{
-                    if(DB::query('SELECT mobile FROM users WHERE mobile=:mobile', array('mobile'=>$email_mobile))){
+                } else {
+                    if (DB::query('SELECT mobile FROM users WHERE mobile=:mobile', array('mobile' => $email_mobile))) {
                         $error = 'Mobile number is already in use.';
-                    }else{
-                        $user_id = $loadFromUser->create('users', array('first_name'=>$first_name, 'last_name'=>$last_name, 'mobile'=> $email_mobile, 'password'=>password_hash($password, PASSWORD_BCRYPT), 'screenName'=>$screenName, 'userLink'=>$userLink, 'birthday'=>$birth, 'gender'=>$upgen));
-                        
+                    } else {
+                        $user_id = $loadFromUser->create('users', array('first_name' => $first_name, 'last_name' => $last_name, 'mobile' => $email_mobile, 'password' => password_hash($password, PASSWORD_BCRYPT), 'screenName' => $screenName, 'userLink' => $userLink, 'birthday' => $birth, 'gender' => $upgen));
+
                         $tstrong = true;
                         $token = bin2hex(openssl_random_pseudo_bytes(64, $tstrong));
-                        $loadFromUser->create('token', array('token'=>$token, 'user_id'=>$user_id));
+                        $loadFromUser->create('token', array('token' => $token, 'user_id' => $user_id));
 
-                        setcookie('FBID', $token, time()+60*60*24*7, '/', NULL, NULL, true);
+                        setcookie('FBID', $token, time() + 60 * 60 * 24 * 7, '/', NULL, NULL, true);
 
                         header('Location: index.php');
                     }
                 }
             }
-        }else{
-            if(!filter_var($email_mobile)){
+        } else {
+            if (!filter_var($email_mobile)) {
                 $error = "Invalid Email Format";
-            }else if(strlen($first_name) > 20){
+            } else if (strlen($first_name) > 20) {
                 $error = "Name must be between 2-20 characters";
-            }else if(strlen($password) <5 && strlen($password) >= 60){
+            } else if (strlen($password) < 5 && strlen($password) >= 60) {
                 $error = "The password is either too short ot too long";
-            }else{
-                if((filter_var($email_mobile,FILTER_VALIDATE_EMAIL)) && $loadFromUser->checkEmail($email_mobile) === true){
+            } else {
+                if ((filter_var($email_mobile, FILTER_VALIDATE_EMAIL)) && $loadFromUser->checkEmail($email_mobile) === true) {
                     $error = "Email is already registered";
-                }else{
-                    
-                    $user_id = $loadFromUser->create('users', array('first_name'=>$first_name, 'last_name'=>$last_name, 'email'=> $email_mobile, 'password'=>password_hash($password, PASSWORD_BCRYPT), 'screenName'=>$screenName, 'userLink'=>$userLink, 'birthday'=>$birth, 'gender'=>$upgen));
-                    
-                    
+                } else {
+
+                    $user_id = $loadFromUser->create('users', array('first_name' => $first_name, 'last_name' => $last_name, 'email' => $email_mobile, 'password' => password_hash($password, PASSWORD_BCRYPT), 'screenName' => $screenName, 'userLink' => $userLink, 'birthday' => $birth, 'gender' => $upgen));
+
+
                     $tstrong = true;
                     $token = bin2hex(openssl_random_pseudo_bytes(64, $tstrong));
-                    $loadFromUser->create('token', array('token'=>$token, 'user_id'=>$user_id));
-                    
-                    setcookie('FBID', $token, time()+60*60*24*7, '/', NULL, NULL, true);
-                    
+                    $loadFromUser->create('token', array('token' => $token, 'user_id' => $user_id));
+
+                    setcookie('FBID', $token, time() + 60 * 60 * 24 * 7, '/', NULL, NULL, true);
+
                     header('Location: index.php');
-                    
                 }
             }
         }
-        
     }
 }
 
@@ -122,7 +120,25 @@ if( isset($_POST['first-name']) && !empty($_POST['first-name'])){
 
 <body>
 
-    <div class="header"></div>
+    <div class="header">
+        <div class="logo">facebook Clone</div>
+        <form action="sign.php" method="post">
+            <div class="sign-in-form">
+                <div class="mobile-input">
+                    <div class="input-text">Email or Phone</div>
+                    <input type="text" name="in-email-mobile" id="email-mobile" class="input-text-field">
+                </div>
+                <div class="password-input">
+                    <div style="font-size: 12px;padding-bottom: 5px;">Password</div>
+                    <input type="text" name="in-pass" id="in-password" class="input-text-field">
+                    <div class="forgotten-acc">Forgotten account?</div>
+                </div>
+                <div class="login-button">
+                    <input type="submit" value="Log in" class="sign-in login">
+                </div>
+            </div>
+        </form>
+    </div>
 
     <div class="main" style="width:100%;">
 
@@ -133,7 +149,9 @@ if( isset($_POST['first-name']) && !empty($_POST['first-name'])){
         <div class="right-side">
 
             <div class="error">
-                <?php if(!empty($error)){ echo $error;}?>
+                <?php if (!empty($error)) {
+                    echo $error;
+                } ?>
             </div>
 
             <h1 style="color:#212121;">Create an account</h1>
@@ -257,7 +275,6 @@ if( isset($_POST['first-name']) && !empty($_POST['first-name'])){
             return new Date(year, month, 0).getDate();
 
         }
-
     </script>
 
 </body>
